a1dfd49df6520ba19c4252d5a1e739e6ac43d310
subha2803 <subha.g2803@gmail.com>
2015-06-08 18:07:53 -0500

    Initial Commit

---
+++ ProductApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProductApp.Startup))]
+namespace ProductApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
